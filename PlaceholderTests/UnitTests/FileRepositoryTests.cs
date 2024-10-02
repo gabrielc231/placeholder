b1a55756a1e988540933157a1abaaa457678fb29
gabrielc231 <142500547+gabrielc231@users.noreply.github.com>
2024-10-01 22:47:30 -0300

    Attempting to run without building

--- PlaceholderTests/UnitTests/FileRepositoryTests.cs
+++ PlaceholderTests/UnitTests/FileRepositoryTests.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using placeholder.Models;
+using placeholder.Models;
 
 namespace placeholder.Tests.UnitTests
 {
